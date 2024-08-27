2f7b4ae8e1dc9bc7ccb853aa0749642d2957ab0d
Brunno Soares <brunno.domingues@outlook.com>
2024-08-27 19:06:54 -0300

    new changes

--- src/Componentes/CardsColecao/CardsColecao.jsx
+++ src/Componentes/CardsColecao/CardsColecao.jsx
@@ -11,24 +11,75 @@ import { CounterContext } from "../Funcoes/Buy";
 import { useContext } from "react";
 
 function CardsColecao() {
-  const {contador} = useContext(CounterContext)
+  const { contador } = useContext(CounterContext);
   const produto = [
-    {sku: "0001", imagem: umimg, nomeproduto: "Conjunto de Tigelas", preco: "R$ 85,00", situacao: "Novo"},
-    {sku: "0002", imagem: doisimg, nomeproduto: "Vaso Cerâmico Preto", preco: "R$ 50,00"},
-    {sku: "0003", imagem: tresimg, nomeproduto: "Vaso Cerâmico Branco", preco: "R$ 40,00"},
-    {sku: "0004", imagem: quatroimg, nomeproduto: "Caneca Vulcânica", preco: "R$ 25,00"},
-    {sku: "0005", imagem: quintoimg, nomeproduto: "Caneca Texturizada", preco: "R$ 38,00", situacao: "Mais vendido"},
-    {sku: "0006", imagem: sextoimg, nomeproduto: "Caneca Serenity", preco: "R$ 18,00"},
-    {sku: "0007", imagem: setimoimg, nomeproduto: "Jarro Minimalista", desconto: "R$ 130,00", preco: "R$ 100,00",situacao: "Promoção"},
-    {sku: "0008", imagem: oitavoimg, nomeproduto: "Conjunto de Bowls", preco: "R$ 45,00"},
-    {sku: "0009", imagem: nonoimg, nomeproduto: "Conjunto de Bowls Rústicos", preco: "R$ 85,00"},
+    {
+      sku: "0001",
+      imagem: umimg,
+      nomeproduto: "Conjunto de Tigelas",
+      preco: "R$ 85,00",
+      situacao: "Novo",
+    },
+    {
+      sku: "0002",
+      imagem: doisimg,
+      nomeproduto: "Vaso Cerâmico Preto",
+      preco: "R$ 50,00",
+    },
+    {
+      sku: "0003",
+      imagem: tresimg,
+      nomeproduto: "Vaso Cerâmico Branco",
+      preco: "R$ 40,00",
+    },
+    {
+      sku: "0004",
+      imagem: quatroimg,
+      nomeproduto: "Caneca Vulcânica",
+      preco: "R$ 25,00",
+    },
+    {
+      sku: "0005",
+      imagem: quintoimg,
+      nomeproduto: "Caneca Texturizada",
+      preco: "R$ 38,00",
+      situacao: "Mais vendido",
+    },
+    {
+      sku: "0006",
+      imagem: sextoimg,
+      nomeproduto: "Caneca Serenity",
+      preco: "R$ 18,00",
+    },
+    {
+      sku: "0007",
+      imagem: setimoimg,
+      nomeproduto: "Jarro Minimalista",
+      desconto: "R$ 130,00",
+      preco: "R$ 100,00",
+      situacao: "Promoção",
+    },
+    {
+      sku: "0008",
+      imagem: oitavoimg,
+      nomeproduto: "Conjunto de Bowls",
+      preco: "R$ 45,00",
+    },
+    {
+      sku: "0009",
+      imagem: nonoimg,
+      nomeproduto: "Conjunto de Bowls Rústicos",
+      preco: "R$ 85,00",
+    },
   ];
-
 
   return (
     <main className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-5 mx-5">
       {produto.map((produtos, index) => (
-        <section key={index} className="relative group cursor-pointer lg:duration-500 lg:hover:scale-102">
+        <section
+          key={index}
+          className="relative group cursor-pointer lg:duration-500 lg:hover:scale-102"
+        >
           <div className="absolute bg-black text-white px-2">
             <p>{produtos.situacao}</p>
           </div>
@@ -40,9 +91,12 @@ function CardsColecao() {
             <p className="text-gray-400 line-through">{produtos.desconto}</p>
             <p className="text-gray-400 ml-4">{produtos.preco}</p>
           </section>
-            <button className=" w-full mt-2 mb-5 p-2 bg-black text-white lg:opacity-0 lg:group-hover:opacity-100 lg:transform lg:group-hover:translate-y-0 lg:translate-y-3 lg:transition-all lg:duration-300" onClick={contador}>
-              Adicionar ao carrinho
-            </button>
+          <button
+            className="w-full mt-2 mb-5 p-2 bg-black text-white lg:opacity-0 lg:group-hover:opacity-100 lg:transform lg:group-hover:translate-y-0 lg:translate-y-3 lg:transition-all lg:duration-300"
+            onClick={contador}
+          >
+            Adicionar ao carrinho
+          </button>
         </section>
       ))}
     </main>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import Home from './Pages/Home'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
